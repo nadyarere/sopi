04615623388ee883084c096acd6c7d5539ace0a0
agus ali hakim <agusalih@gmail.com>
2022-11-10 11:32:01 +0700

    test

---
+++ seeders/20221110032959-seedUser.js
@@ -0,0 +1,40 @@
+'use strict';
+const fs= require('fs')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    const users = JSON.parse(fs.readFileSync('./datas/users.json', 'utf-8')).map(el => {
+      delete el.id
+
+      el.createdAt = new Date()
+      el.updatedAt = new Date()
+
+      return el
+    })
+
+    return queryInterface.bulkInsert("Users", users, {})
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+     return queryInterface.bulkDelete("Users", null, {})
+
+  }
+};
