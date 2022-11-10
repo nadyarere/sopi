f05eb0c2d8f00bcf6e9eccb0ff61acf7b7af9338
Nadya <nadya.rere@gmail.com>
2022-11-10 11:24:06 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const bcrypt = require('bcryptjs');
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+// / landing page logo deskripsi ada loginnya ada register tombol
+
+
+app.get('/login', (req, res) => {
+    res.render('loginPage')
+})
+
+app.post('/login', (req, res) => {
+    res.send(req.body)
+})
+
+app.get('/register', (req, res) => {
+    res.render('registerPage')
+})
+
+app.post('/register', (req, res) => {
+    res.send(req.body)
+})
+
+
+//userid pake session
+/**
+ * /products list kategory dan products yang diliat sama customer klo admin 
+ * /products/:productId (detail product)
+ * /checkout (dilihat sama customer) //create order
+ * /orders (diliat admin) //findall order
+ * /products/add (diliat admin)
+ * /products/:productId/delete (diliat admin)
+ * /products/:productId/edit (admin)
+ */
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
